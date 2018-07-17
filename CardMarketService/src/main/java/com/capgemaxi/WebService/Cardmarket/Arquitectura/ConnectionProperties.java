91b3e99a444458b8a6c7e73efbb4e09ffb850c04
saul.blanco <saul.blanco@tlucc181.treelogic.local>
2018-07-17 12:08:02 +0200

    commit

---
+++ CardMarketService/src/main/java/com/capgemaxi/WebService/Cardmarket/Arquitectura/ConnectionProperties.java
@@ -0,0 +1,9 @@
+package com.capgemaxi.WebService.Cardmarket.Arquitectura;
+
+public class ConnectionProperties {
+
+	public static final String mkmAppToken = "VcCigHQVEOqiftlI" ;
+	public static final String mkmAppSecret = "QIOg2b0GWvLX1LRV2NlygFKwyzn8Brp1" ;
+	public static final String mkmAccessToken = "um6OGMVy0po7ZINCa8cSEHHCo0yjhVFJ" ;
+	public static final String mkmAccessTokenSecret = "G9IUsEXVUZshO8hqqdIJfocZ0dGd3bLp" ;
+}
---
+++ CardMarketService/src/main/java/com/capgemaxi/WebService/Cardmarket/WSObtenerInformacionCartas.java
@@ -0,0 +1,19 @@
+package com.capgemaxi.WebService.Cardmarket;
+
+import com.capgemaxi.WebService.Cardmarket.Arquitectura.ConnectionProperties;
+
+public class WSObtenerInformacionCartas extends ConnectionProperties{
+
+	/**
+	 * devuelve el precio Minimo de una carta, si hay varias expansiones coge el mas bajo
+	 * @param carta
+	 * @param idioma
+	 * @return
+	 */
+
+	public Number obtenerPrecioMinimoCarta(String carta, int idioma) {
+		return idioma;
+
+	}
+
+}
